22c573f62ff47b57dca711fcca1953ab362f1729
Eduard0606 <zalyaev2001@list.ru>
2024-09-03 01:41:15 +0300

    header__user_info

--- makeupdate/src/components/header/Header.jsx
+++ makeupdate/src/components/header/Header.jsx
@@ -7,6 +7,9 @@ import userIcon from "./../../img/Group 83.svg";
 import Language from "./../../img/Russian.svg";
 import notification from "./../../img/Главная.svg";
 import hamburger_menu from "./../../img/Гамбургер меню.svg";
+import userLogo from "./../../img/User_logo.svg";
+import place from "./../../img/Местоположение.svg";
+import player from "./../../img/Group 102.svg";
 
 function Header() {
   return (
@@ -38,7 +41,6 @@ function Header() {
             <div className="container_elements">
               <img src={Language} alt="Язык" />
             </div>
-
             <div className="container_elements">
               <img src={userIcon} alt="Юзеры" />
             </div>
@@ -47,7 +49,27 @@ function Header() {
             </div>
           </div>
         </div>
-        <div></div>
+        <div className="container__names">
+          <div className="firstName">ИМЯ</div>
+          <div className="firstName">ФАМИЛИЯ</div>
+        </div>
+
+        <div className="container__user_img">
+          <img src={userLogo} alt="Фотография пользователя" className="img" />
+          <div className="user_info_cont">
+           <span className="place"> <img src={place} alt="Местоположение" />Не задано</span>
+         <div className="container__views">
+          <span className="player"> <img src={player} alt="Плеер" />0</span>
+          <span className="player"> <img src={player} alt="Плеер" />0</span>
+         </div>
+         <button className="btn1">Редактировать</button>
+         <button>Опубликовать урок</button>
+         <button>Опубликовать событие</button>
+         <button>Опубликовать фото</button>
+         <div className="about"><b>Обо мне:</b></div>
+         <div className="about2">Заполните описание</div>
+        </div>
+        </div>
       </div>
     </header>
   );
---
+++ makeupdate/src/components/user_info/Userinfo.jsx
@@ -0,0 +1,10 @@
+const UserInfo = () => {
+    return ( 
+     <section className="userinfo">
+
+     </section>
+
+    );
+}
+ 
+export default UserInfo;
